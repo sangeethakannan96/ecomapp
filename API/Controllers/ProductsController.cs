b2ecf4e5519eecdd98374c3ddfb737e32e35b556
Sangeetha Kannan <Sangeetha.Kannan.ext@sodexo.com>
2023-12-21 21:44:30 +0530

    Auto mapper and static files

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
@@ -22,30 +23,24 @@ namespace API.Controllers
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
         private readonly IGenericRepository<ProductType> _productTypeRepo;
+        private readonly IMapper _mapper;
 
         public ProductsController(IGenericRepository<Product> productsRepo, 
-        IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType> productTypeRepo)
+        IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType> productTypeRepo,
+        IMapper mapper)
         {
             _productsRepo = productsRepo;
             _productBrandRepo = productBrandRepo;
             _productTypeRepo = productTypeRepo;
+            _mapper = mapper;
         }
         
        [HttpGet]
-       public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts(){
+       public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(){
         
         var spec = new ProductsWithTypesAndBrandsSpecification();
         var products =  await _productsRepo.ListAsync(spec);
-        return products.Select(product => new ProductToReturnDto
-        {
-           Id = product.Id,
-            Name = product.Name,
-            Description = product.Description,
-            PictureUrl = product.PictureUrl,
-            Price = product.Price,
-            ProductBrand = product.ProductBrand.Name,
-            ProductType = product.ProductType.Name  
-        }).ToList();
+        return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
 
        }
 
@@ -55,15 +50,7 @@ namespace API.Controllers
        {
         var spec = new ProductsWithTypesAndBrandsSpecification(id);
         var product = await _productsRepo.GetEntityWithSpec(spec);
-        return new ProductToReturnDto{
-            Id = product.Id,
-            Name = product.Name,
-            Description = product.Description,
-            PictureUrl = product.PictureUrl,
-            Price = product.Price,
-            ProductBrand = product.ProductBrand.Name,
-            ProductType = product.ProductType.Name
-        };
+        return _mapper.Map<Product, ProductToReturnDto>(product);
 
        }
 
--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
  });
  builder.Services.AddScoped<IProductRepository, ProductRepository>();
  builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -28,7 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();

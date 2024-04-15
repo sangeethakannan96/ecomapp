2bf1b9bd17591796b523c14899f1b84347cacbb1
Sangeetha Kannan <Sangeetha.Kannan.ext@sodexo.com>
2024-04-15 16:07:32 +0530

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -24,6 +24,7 @@ app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();

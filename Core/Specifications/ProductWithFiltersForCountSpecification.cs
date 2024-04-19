82d6446cb0d712701985985f3c42d29d853e4be0
Sangeetha Kannan <Sangeetha.Kannan.ext@sodexo.com>
2024-04-19 16:50:26 +0530

    Client Application

---
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Specification;
+
+namespace Core.Specifications
+{
+    public class ProductWithFiltersForCountSpecification : BaseSpecification<Product>
+    {
+        public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
+        : base(x =>
+         (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+         (productParams.Search)) &&
+         (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+         (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+                )
+        {
+            
+        }
+    }
+}
